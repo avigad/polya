d6932f94b4ad925d2c8705dab2f1901b751bb068
Jeremy Avigad <avigad@cmu.edu>
2014-01-28 20:18:40 -0500

    removed unicode

--- examples/other_systems/isabelle_problems.thy
+++ examples/other_systems/isabelle_problems.thy
@@ -12,7 +12,7 @@ imports Complex_Main
 begin
 
 (* sledgehammer and auto fail on this *)
-lemma "(0::real) < u ==> u < v ==> v < 1 ==> 2 <= x \<Longrightarrow> x <= y \<Longrightarrow> 2 * u * v^2 < v * y^2"
+lemma "(0::real) < u ==> u < v ==> v < 1 ==> 2 <= x ==> x <= y ==> 2 * u * v^2 < v * y^2"
   apply (simp add: field_simps eval_nat_numeral)
 by (rule mult_strict_mono, auto)
 
@@ -78,7 +78,8 @@ lemma "(ALL x. f x <= 2) ==> (0::real) < w ==> u < v ==> u + w * (f x - 1) < v +
 (* sledgehammer finds a solution using resolution *)
 lemma "(0::real) < x ==> x < y ==> u < v ==>
     2 * u + exp (1 + x + x^4) <= 2 * v + exp (1 + y + y^4)"
-by (metis add_less_cancel_left add_mono_thms_linordered_field(5) exp_less_cancel_iff less_eq_real_def mult_2 power_strict_mono zero_less_numeral)
+by (metis add_less_cancel_left add_mono_thms_linordered_field(5) exp_less_cancel_iff 
+    less_eq_real_def mult_2 power_strict_mono zero_less_numeral)
 
 (* sledgehammer finds a solution using Z3 *)
 lemma "(0::real) < x ==> x < y ==> u < v ==>
